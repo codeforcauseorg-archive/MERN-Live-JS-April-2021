a3fc4dbc6608e179072e660613f05a32e143da34
keenwarrior <anujgargcse@gmail.com>
2021-05-01 17:49:44 +0530

    l 7

--- lecture-07/helloreact/src/index.js
+++ lecture-07/helloreact/src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Mat } from "./Mat";
+// import App from './App'
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Mat />, document.getElementById("root"));
