d8b8e1e1c37f5968c70a9e829b7abd90e470908c
keenwarrior <anujgargcse@gmail.com>
2021-05-13 17:53:34 +0530

    up

--- lecture-12/firelogin/src/Routes.js
+++ lecture-12/firelogin/src/Routes.js
@@ -2,6 +2,8 @@ import { Switch, Route } from "react-router-dom";
 import AuthGaurd from "./components/AuthGaurd";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import Inbox from "./pages/Inbox";
+import Draft from "./pages/Draft";
 import Login from "./pages/Login";
 
 export default function Routes() {
@@ -14,6 +16,20 @@ export default function Routes() {
         <AuthGaurd>
           <MainLayout>
             <Home />
+          </MainLayout>
+        </AuthGaurd>
+      </Route>
+      <Route path="/inbox" exact>
+        <AuthGaurd>
+          <MainLayout>
+            <Inbox />
+          </MainLayout>
+        </AuthGaurd>
+      </Route>
+      <Route path="/draft" exact>
+        <AuthGaurd>
+          <MainLayout>
+            <Draft />
           </MainLayout>
         </AuthGaurd>
       </Route>
