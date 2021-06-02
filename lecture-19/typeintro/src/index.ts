91f252b65c11d62753c9a2e793bd9242ff5c365b
keenwarrior <anujgargcse@gmail.com>
2021-06-02 17:07:50 +0530

    l 19

---
+++ lecture-19/typeintro/src/index.ts
@@ -0,0 +1,43 @@
+// let a : number = 10;
+
+// function hello(a: any, b?: any) {
+//   console.log(a, b);
+// }
+
+// hello("anuj");
+
+// type Point = {
+//   x: number;
+//   y: number;
+// };
+
+// let item: Point = {
+//   x: 67,
+//   y: 56,
+// };
+
+// const user = {
+//   name: "Daniel",
+//   age: 26,
+//   location: "hh"
+// };
+
+// user.location;
+
+// function flipCoin() {
+//     // Meant to be Math.random()
+//     return Math.random < 0.5;
+//   }
+
+// function hello(a: any, b?: any): void {
+//   console.log(a, b);
+// }
+
+// let a = hello("he");
+// console.log(a);
+
+let item: { x: number; y: number } = { x: 10, y: 10 };
+
+type caller = number | boolean;
+
+let something: caller = true;
---
+++ lecture-19/typeintro/src/practice.js
@@ -0,0 +1,10 @@
+// function hello(a, b) {
+//   console.log(a, b);
+// }
+
+// hello("anuj");
+
+function flipCoin() {
+  // Meant to be Math.random()
+  return Math.random < 0.5;
+}
