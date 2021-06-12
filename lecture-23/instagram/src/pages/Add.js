2da064c1cc2e9a57f5e1b3d0846a8b72526abbe7
keenwarrior <anujgargcse@gmail.com>
2021-06-12 18:05:10 +0530

    l 23

--- lecture-23/instagram/src/pages/Add.js
+++ lecture-23/instagram/src/pages/Add.js
@@ -1,17 +1,26 @@
 import { Box } from "@material-ui/core";
-import firebase from "../utils/firebase";
+import { useState } from "react";
+import ImageUploader from "react-images-upload";
 
 export default function Add() {
+  let [picture, setPicture] = useState();
+
+  let onDrop = function (pictures) {
+    console.log(pictures[pictures.length - 1]);
+    setPicture(pictures[pictures.length - 1]);
+  };
+
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      <h1>Add Page</h1>
-      <button
-        onClick={function () {
-          firebase.auth().signOut();
-        }}
-      >
-        Sign Out
-      </button>
+      <ImageUploader
+        withIcon={true}
+        buttonText="Choose images"
+        onChange={onDrop}
+        imgExtension={[".jpg", ".jpeg", ".gif", ".png", ".gif"]}
+        maxFileSize={5242880}
+      />
+
+      {picture && <img src={URL.createObjectURL(picture)} alt="something" />}
     </Box>
   );
 }
